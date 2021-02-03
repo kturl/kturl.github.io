967c7ad51c5f112dcd5af24a237a6448bc43ab60
Kev <kturley@gmail.com>
2021-02-03 00:56:32 -0500

    updated Codenames

--- codenames/lib/main.js
+++ codenames/lib/main.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll('.card');
 //word list in all CAPS
 const endpoint = 'https://raw.githubusercontent.com/kturl/codenames/master/words.json';
 
+
 //Event Listeners
 cards.forEach(card => card.addEventListener('click', selectRed));
 cards.forEach(card => card.addEventListener('dblclick', selectBlue));
@@ -14,3 +15,21 @@ function selectRed() {
 function selectBlue() {
   this.classList.toggle('choseBlue');
 }
+
+//STEPS
+// 1) get an array of 25 words at random from the json
+//  -) access json
+//  -) randomly create an array of 25 integers (0 to '1 - json.length')
+//  -) select the words at those positions and save in the same array
+
+// 2) assign the words each to tile
+//  -) use the array to iterate words into tiles
+
+// 3) give card properties (red/blue/black/neutral) to the tiles
+//  -) figure out game board patterns and assign to an array
+//  -) combine game board properties with word array
+
+// 5) evaluate choice, return results
+//  -) the array properties will be checked at each tile selection event for true:false to determine outcome
+
+// 4) indicate who plays first and initiate turn
