d64241edc39bed023a6f6ff2bdcae82fae08dca4
Kev <kturley@gmail.com>
2021-03-22 20:32:14 -0400

    cleaned up code a bit, checklist "complete"

--- checklist/lib/script.js
+++ checklist/lib/script.js
@@ -1,19 +1,19 @@
-const submitButton = document.getElementById('submitButton');
-const staplesButton = document.getElementById('staplesButton');
-const clearButton = document.getElementById('clearButton');
-const userInput = document.getElementById('userInput');
-const ul = document.getElementById('list');
-const sl = document.getElementById('sideList');
+const submitButton = document.getElementById('submitButton')
+const staplesButton = document.getElementById('staplesButton')
+const clearButton = document.getElementById('clearButton')
+const userInput = document.getElementById('userInput')
+const ul = document.getElementById('list')
+const sl = document.getElementById('sideList')
 
-let todoList = [];
-const staples = ['spinach', 'lettuce', 'peppers', 'onions', 'garlic', 'apples', 'bananas', 'limes', 'bread', 'bacon', 'beef', 'chicken', 'lunch meat', 'cheese', 'cottage cheese', 'sour cream', 'yogurt', 'eggs'];
-let savedTodos;
-let draggables = [];
-let containers;
+let todoList = []
+const staples = ['spinach', 'lettuce', 'peppers', 'onions', 'garlic', 'apples', 'bananas', 'limes', 'bread', 'bacon', 'beef', 'chicken', 'lunch meat', 'cheese', 'cottage cheese', 'sour cream', 'yogurt', 'eggs']
+let savedTodos
+let draggables = []
+let containers
 
 function store() {
-  savedTodos = JSON.stringify(todoList);
-  localStorage.setItem('todoList', savedTodos);
+  savedTodos = JSON.stringify(todoList)
+  localStorage.setItem('todoList', savedTodos)
 }
 
 function addTodo(text) {
@@ -24,30 +24,30 @@ function addTodo(text) {
     id: Math.ceil(Math.random() * 1000000)
   };
   //avoid doubles by using todoList.map() to get an array of just the text property and check if the new item is present
-  let items = todoList.map(a => a.text);
+  let items = todoList.map(a => a.text)
   //if it is not present, add it to the list
   if (!items.includes(todo.text)) {
-    todoList.push(todo);
-    renderTodo(todo);
-    store();
+    todoList.push(todo)
+    renderTodo(todo)
+    store()
     
   }
 }
 
-staplesButton.addEventListener('click', addStaples);
+staplesButton.addEventListener('click', addStaples)
 function addStaples() {
   for (let i = 0; i < staples.length; i++) {
-    addTodo(staples[i]);
+    addTodo(staples[i])
   }
   // staples.forEach(item => addTodo(item));
   // userInput.focus();
 }
 
 function toggleDone(key) {
-  const index = todoList.findIndex(item => item.id === Number(key));
-  todoList[index].checked = !todoList[index].checked;
-  renderTodo(todoList[index]);
-  store();
+  const index = todoList.findIndex(item => item.id === Number(key))
+  todoList[index].checked = !todoList[index].checked
+  renderTodo(todoList[index])
+  store()
 }
 
 function removeTodo(key) {
@@ -56,34 +56,34 @@ function removeTodo(key) {
     deleted: true,
     ...todoList[index]
   }
-  todoList = todoList.filter(item => item.id !== Number(key));
-  renderTodo(todo);
-  store();
+  todoList = todoList.filter(item => item.id !== Number(key))
+  renderTodo(todo)
+  store()
 }
 
-const form = document.getElementById('myForm');
+const form = document.getElementById('myForm')
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  const text = userInput.value.trim();
+  e.preventDefault()
+  const text = userInput.value.trim()
   if (text !== '') {
-    addTodo(text.toLowerCase());
-    userInput.value = '';
-    userInput.focus();
+    addTodo(text.toLowerCase())
+    userInput.value = ''
+    userInput.focus()
   }
-});
+})
 
 function renderTodo(todo) {
-  const isChecked = todo.checked ? 'done' : '';
-  const item = document.querySelector(`[data-key='${todo.id}']`);
-  let li = document.createElement('li');
-  li.setAttribute('class', `item ${isChecked} draggable`);
-  li.setAttribute('draggable', `true`);
-  li.setAttribute('data-key', todo.id);
+  const isChecked = todo.checked ? 'done' : ''
+  const item = document.querySelector(`[data-key='${todo.id}']`)
+  let li = document.createElement('li')
+  li.setAttribute('class', `item ${isChecked} draggable`)
+  li.setAttribute('draggable', `true`)
+  li.setAttribute('data-key', todo.id)
   li.innerHTML = `
   <div id="${todo.id}" class="itemButton checkBox"></div>
   <span class="item">${todo.text}</span>
   <div id="delete${todo.id}" class="itemButton delete">X</div>
-  `;
+  `
   
   if (todo.deleted) {
     item.remove()
@@ -101,45 +101,43 @@ function renderTodo(todo) {
 }
 ul.addEventListener('click', e => {
   if (e.target.classList.contains('item') || e.target.classList.contains('checkBox')) {
-    const itemKey = e.target.parentElement.dataset.key;
-    toggleDone(itemKey);
+    const itemKey = e.target.parentElement.dataset.key
+    toggleDone(itemKey)
   }
-});
+})
 ul.addEventListener('click', e => {
   if (e.target.classList.contains('delete')){
-    const itemKey = e.target.parentElement.dataset.key;
+    const itemKey = e.target.parentElement.dataset.key
     removeTodo(itemKey);
   }
-});
+})
 
 function clearList() {
-  ul.innerHTML = '';
+  ul.innerHTML = ''
 //setting the array.length to 0 clears it easily. Could otherwise .splice(0, array.length). Both are similar in performance.
-  todoList.length = 0;
-  store();
+  todoList.length = 0
+  store()
 }
-clearButton.addEventListener('click', clearList);
+clearButton.addEventListener('click', clearList)
 
 window.addEventListener('load', () => {
-  const ref = localStorage.getItem('todoList');
+  const ref = localStorage.getItem('todoList')
   if (ref) {
     savedTodoList = JSON.parse(ref);
     savedTodoList.forEach(todo => {
-      let items = todoList.map(a => a.text);
+      let items = todoList.map(a => a.text)
       if (!items.includes(todo.text)) {
-        todoList.push(todo);
-        renderTodo(todo);
+        todoList.push(todo)
+        renderTodo(todo)
       }
-    });
+    })
   }
-});
-//ADD SHOW/NOT SHOW BUTTON FOR 'STAPLES' LIST
-//ADD FUNCTION TO SORT BY FOOD TYPE (add property)
+})
 
 //MAKES THE ITEMS DRAGGABLE AND RE-ARRANGABLE
 function setDrag(item) {
   draggables = document.querySelectorAll('.draggable')
-  // console.log(draggables)
+  //FOR PC
   item.addEventListener('dragstart', () => {
     console.log('Dragging...')
     item.classList.toggle('dragging')
@@ -148,6 +146,7 @@ function setDrag(item) {
     console.log('Done')
     item.classList.toggle('dragging')
   })
+  //FOR MOBILE (TOUCH SCREEN)
   item.addEventListener('touchstart', () => {
     console.log('Dragging...')
     item.classList.toggle('dragging')
@@ -159,10 +158,10 @@ function setDrag(item) {
 }
 
 container = document.querySelector('.container')
+//FOR PC
 container.addEventListener('dragover', e => {
   const draggable = document.querySelector('.dragging')
   const afterElement = getDragAfterElement(container, e.clientY)
-  // console.log(afterElement)
   e.preventDefault()
   if (afterElement == null) {
     container.appendChild(draggable)
@@ -170,6 +169,7 @@ container.addEventListener('dragover', e => {
     container.insertBefore(draggable, afterElement)
   }
 })
+//FOR MOBILE (TOUCH SCREEN)
 container.addEventListener('touchmove', e => {
   const draggable = document.querySelector('.dragging')
   const touchLocation = e.targetTouches[0]
@@ -187,7 +187,6 @@ function getDragAfterElement(container, y) {
   return draggableElements.reduce((closest, child) => {
     const box = child.getBoundingClientRect()
     const offset = y - box.top - box.height / 2
-    // console.log(closest)
     if (offset < 0 && offset > closest.offset) {
       return { offset: offset, element: child }
     } else {
